21493ff556db65b22cbdfd7dcfb9f7080d5a1504
FabioMartins <fabbio1000@gmail.com>
2023-01-30 20:48:00 -0300

    Feat: Criptografia de inversões

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message("a", "b")
+        encrypt_message(1, 2)
+    
+    assert encrypt_message("segredo", 3) == "ges_oder"
+    assert encrypt_message("segredo", 9) == "oderges"
+    assert encrypt_message("segredo", 4) == "ode_rges"
+    assert encrypt_message("segredo", 5) == "erges_od"
+    assert encrypt_message("segredo", 6) == "o_derges"
