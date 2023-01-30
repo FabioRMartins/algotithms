424ccfc67a760eab16b241746c9619bf49d1cbef
FabioMartins <fabbio1000@gmail.com>
2023-01-30 19:21:00 -0300

    Feat: Número de estudantes estudando no mesmo horário

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,16 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    counter = 0
+
+    if permanence_period is None or target_time is None:
+        return None
+
+    for a, z in permanence_period:
+        if not all(
+            all(
+                isinstance(i, int) for i in tuple)
+                for tuple in permanence_period):
+            return None
+        if a <= target_time <= z:
+            counter += 1
+
+    return counter
