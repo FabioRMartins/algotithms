4764a0c465771402c77f5bb840ba65dcf454904b
FabioMartins <fabbio1000@gmail.com>
2023-01-30 19:37:09 -0300

    Fix: Corrigindo estrutura do código

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -4,13 +4,12 @@ def study_schedule(permanence_period, target_time):
     if permanence_period is None or target_time is None:
         return None
 
+    if not all(
+        all(
+            isinstance(i, int) for i in tuple) for tuple in permanence_period):
+        return None
+
     for a, z in permanence_period:
-        if not all(
-            all(
-                isinstance(i, int) for i in tuple)
-                for tuple in permanence_period):
-            return None
         if a <= target_time <= z:
             counter += 1
-
     return counter
