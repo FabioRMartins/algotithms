dcd0d46d9562019a9893b3037e70b3214ec9ff3e
FabioMartins <fabbio1000@gmail.com>
2023-01-30 23:20:44 -0300

    Feat: Anagramas (Algoritmo de ordenação)

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,39 @@
+def merge_sort(array):
+    if len(array) > 1:
+        middle = len(array) // 2
+        left = merge_sort(array[:middle])
+        right = merge_sort(array[middle:])
+        i = j = k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                array[k] = left[i]
+                i += 1
+            else:
+                array[k] = right[j]
+                j += 1
+            k += 1
+        if i < len(left):
+            array[k] = left[i]
+            i += 1
+            k += 1
+        else:
+            j < len(right)
+            array[k] = right[j]
+            j += 1
+            k += 1
+    return array
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    first_string = list(first_string.lower())
+    second_string = list(second_string.lower())
+    sort_first_string = "".join(merge_sort(first_string))
+    sort_second_string = "".join(merge_sort(second_string))
+
+    if not first_string and not second_string:
+        return ('', '', False)
+    if sort_first_string == sort_second_string:
+        return (sort_first_string, sort_second_string, True)
+    else:
+        return (sort_first_string, sort_second_string, False)
